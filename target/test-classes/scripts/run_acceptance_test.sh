bd9a255c5dda06e8a6c6ddbdb563f477c558ae24
Abhi55521 <abhi55521@gmail.com>
2021-11-06 10:45:53 +0530

    Update run_acceptance_test.sh
    Added string test case

--- target/test-classes/scripts/run_acceptance_test.sh
+++ target/test-classes/scripts/run_acceptance_test.sh
@@ -16,3 +16,14 @@ echo formed url $curl_url
 set -e
 curl -sf -o /dev/null $curl_url
 echo "$curl_url is working fine"
+Runtime r = Runtime.getRuntime();
+String[] cmd = {"java", "MyTest"};
+String[] env = {"-classpath", testsDir + "/classes"};
+Process p = r.exec(cmd, env);
+// read output from test using
+// p.getInputStream() and p.getErrorStream()
+// (best done with a separate thread for each stream)
+int rc = p.waitFor();
+Status s = (rc == 0 ? Status.passed("OK") : 
+Status.failed("process exited with return code " + rc);
+// s contains result status from executing command
