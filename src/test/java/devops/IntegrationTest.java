b411045a041c6213247a8d0756b0fd8f608e37c9
ss-acc <93214201+ss-acc@users.noreply.github.com>
2021-11-06 11:28:58 +0530

    Update IntegrationTest.java

--- src/test/java/devops/IntegrationTest.java
+++ src/test/java/devops/IntegrationTest.java
@@ -8,5 +8,9 @@ public class IntegrationTest extends TestCase {
   public void testSampleTest1() {
     System.out.println("some sample test");
   }
+  @Test
+  public void testSampleTest2() {
+    System.out.println("sample test2");
+  }
 
 }
